15693eef88aac3d19be9d53cb53dc48e31e9b575
anaschaeru <anasmaulana9@gmail.com>
2025-06-17 01:59:16 +0700

    assets

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -7,8 +7,7 @@
     <title>Antrian SPMB SMKN 4 Tangerang</title>
 
     <!-- Combined and optimized CSS -->
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.6/dist/css/bootstrap.min.css" rel="stylesheet"
-        integrity="sha384-4Q6Gf2aSP4eDXB8Miphtr37CMZZQ5oXLH2yaXMJ2w8e2ZtHTl7GptT4jmndRuHDT" crossorigin="anonymous">
+    <link rel="stylesheet" href="{{ asset('css/bootstrap.min.css') }}">
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.13.1/font/bootstrap-icons.min.css">
     <link rel="stylesheet" href="{{ asset('css/dataTables.bootstrap5.css') }}">
     <link rel="stylesheet" href="{{ asset('css/custom.css') }}">
@@ -36,10 +35,7 @@
         </div>
     </footer>
 
-    <!-- Combined and optimized JS -->
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.6/dist/js/bootstrap.bundle.min.js"
-        integrity="sha384-j1CDi7MgGQ12Z7Qab0qlWQ/Qqz24Gc6BM0thvEMVjHnfYGF0rmFCozFSxQBxwHKO" crossorigin="anonymous">
-    </script>
+    <script src="{{ asset('js/bootstrap.bundle.min.js') }}"></script>
     <script src="{{ asset('js/jquery-3.7.1.js') }}"></script>
     <script src="{{ asset('js/dataTables.js') }}"></script>
     <script src="{{ asset('js/dataTables.bootstrap5.js') }}"></script>
