598f0c0a404c191e03306521997332f6a86b0de5
anaschaeru <anasmaulana9@gmail.com>
2025-06-24 04:45:00 +0700

    update tes minat bakat

--- resources/views/tes-minat-bakat.blade.php
+++ resources/views/tes-minat-bakat.blade.php
@@ -9,8 +9,8 @@
         </div>
         <div class="col-md-12">
             <div class="mb-4">
-                <h3 class="fw-bolder">Jadwal Tes Minat Bakat</h3>
-                <p>Silakan cek data tes minat bakat Anda di bawah ini.</p>
+                <h3 class="fw-bolder text-center">Jadwal Tes Minat Bakat</h3>
+                <p class="text-center">Silakan cek data tes minat bakat Anda di bawah ini.</p>
                 <div class="card mb-4">
                     <div class="card-header header">
                         <h4 class="mb-0 py-2">Informasi Tes Minat Bakat</h4>
@@ -91,48 +91,49 @@
             <div class="card">
                 <div class="card-body">
 
-
-                    <table id="tableMinatBakat" class="table table-striped">
-                        <thead class="table-dark">
-                            <tr class="text-center align-middle">
-                                <th>#</th>
-                                <th>Nama Pendaftar</th>
-                                <th>Asal Sekolah</th>
-                                <th class="text-center">Pilihan Konsentrasi ke- 1</th>
-                                <th>Tes Minat Bakat</th>
-                            </tr>
-                        </thead>
-                        <tbody>
-                            @php
-                                $konsentrasiMapping = [
-                                    'TP' => 'Teknik Pemesinan',
-                                    'TMI' => 'Teknik Mekanik Industri',
-                                    'DGM' => 'Desain Gambar Mesin',
-                                    'TITL' => 'Teknik Instalasi Tenaga Listrik',
-                                    'TOI' => 'Teknik Otomasi Industri',
-                                    'DPIB' => 'Desain Pemodelan dan Informasi Bangunan',
-                                    'TKP' => 'Teknik Konstruksi dan Perumahan',
-                                    'TPG' => 'Teknik Perawatan Gedung',
-                                    'TG' => 'Teknik Geomatika',
-                                    'RPL' => 'Rekayasa Perangkat Lunak',
-                                ];
-                            @endphp
-                            @foreach ($testMinatBakat as $item)
-                                <tr class="align-middle ">
-                                    <td class="text-center">{{ $loop->iteration }}</td>
-                                    <td class="text-uppercase">{{ $item->nomor_formulir }}<br>{{ $item->nama }}
-                                    </td>
-                                    <td class="text-start">{{ $item->asal_sekolah }}</td>
-                                    <td class="text-center">
-                                        {{ $konsentrasiMapping[$item->konsentrasi_1] ?? 'Belum Memilih' }}</td>
-                                    <td>
-                                        Ruang: {{ $item->ruang_tes ?? 'Belum ditentukan' }}
-                                        Sesi: {{ $item->sesi_tes ?? 'Belum ditentukan' }}
-                                    </td>
+                    <div class="table-responsive-md">
+                        <table id="tableMinatBakat" class="table table-striped">
+                            <thead class="table-dark">
+                                <tr class="text-center align-middle">
+                                    <th>#</th>
+                                    <th>Nama Pendaftar</th>
+                                    <th>Asal Sekolah</th>
+                                    <th class="text-center">Pilihan Konsentrasi ke- 1</th>
+                                    <th>Tes Minat Bakat</th>
                                 </tr>
-                            @endforeach
-                        </tbody>
-                    </table>
+                            </thead>
+                            <tbody>
+                                @php
+                                    $konsentrasiMapping = [
+                                        'TP' => 'Teknik Pemesinan',
+                                        'TMI' => 'Teknik Mekanik Industri',
+                                        'DGM' => 'Desain Gambar Mesin',
+                                        'TITL' => 'Teknik Instalasi Tenaga Listrik',
+                                        'TOI' => 'Teknik Otomasi Industri',
+                                        'DPIB' => 'Desain Pemodelan dan Informasi Bangunan',
+                                        'TKP' => 'Teknik Konstruksi dan Perumahan',
+                                        'TPG' => 'Teknik Perawatan Gedung',
+                                        'TG' => 'Teknik Geomatika',
+                                        'RPL' => 'Rekayasa Perangkat Lunak',
+                                    ];
+                                @endphp
+                                @foreach ($testMinatBakat as $item)
+                                    <tr class="align-middle ">
+                                        <td class="text-center">{{ $loop->iteration }}</td>
+                                        <td class="text-uppercase">{{ $item->nomor_formulir }}<br>{{ $item->nama }}
+                                        </td>
+                                        <td class="text-start">{{ $item->asal_sekolah }}</td>
+                                        <td class="text-center">
+                                            {{ $konsentrasiMapping[$item->konsentrasi_1] ?? 'Belum Memilih' }}</td>
+                                        <td>
+                                            Ruang: {{ $item->ruang_tes ?? 'Belum ditentukan' }}
+                                            Sesi: {{ $item->sesi_tes ?? 'Belum ditentukan' }}
+                                        </td>
+                                    </tr>
+                                @endforeach
+                            </tbody>
+                        </table>
+                    </div>
                 </div>
             </div>
         </div>
